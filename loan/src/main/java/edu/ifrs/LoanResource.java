f965b5dfce774b77fbbb9e4350ada01b98de2761
victoriatrois <victoriatrois@outlook.com>
2026-09-13 08:08:59 -0300

    Initial commit

---
+++ loan/src/main/java/edu/ifrs/LoanResource.java
@@ -0,0 +1,16 @@
+package edu.ifrs;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/loan")
+public class LoanResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+}
---
+++ loan/src/test/java/edu/ifrs/LoanResourceTest.java
@@ -0,0 +1,20 @@
+package edu.ifrs;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class LoanResourceTest {
+    @Test
+    void testHelloEndpoint() {
+        given()
+          .when().get("/loan")
+          .then()
+             .statusCode(200)
+             .body(is("Hello from Quarkus REST"));
+    }
+
+}
